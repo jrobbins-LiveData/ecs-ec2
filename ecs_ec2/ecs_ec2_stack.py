6fd84ebe884d7934cdb4ea66b2e58dc395353e91
Jeff Robbins <jeffr@livedata.com>
2021-04-10 12:57:05 -0400

    first attempt at stack

--- ecs_ec2/ecs_ec2_stack.py
+++ ecs_ec2/ecs_ec2_stack.py
@@ -1,15 +1,44 @@
+# based on https://blog.kylegalbraith.com/2019/10/24/how-to-run-docker-containers-via-aws-elastic-container-service/
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_ecr as ecr
+from aws_cdk import aws_ecs as ecs
+from aws_cdk import aws_ecs_patterns as ecs_patterns
 from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
 
 
 class EcsEc2Stack(cdk.Stack):
-
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # ECR repository
+        repository = ecr.Repository(self, 'myWebApp',
+          repository_name='myWebApp'
+        )
+
+        # ECS cluster/resources
+        cluster = ecs.Cluster(self, 'myWebApp-cluster',
+          cluster_name='myWebApp-cluster'
+        )
+
+        # https://aws.amazon.com/ec2/graviton/
+        # Free trial until June 30th 2021
+        cluster.add_capacity('myWebApp-scaling-group',
+          instance_type=ec2.InstanceType('t4g.micro'),
+          desired_capacity=1
+        )
+
+        # https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ecs_patterns/ApplicationLoadBalancedEc2Service.html
+        load_balanced_service = ecs_patterns.ApplicationLoadBalancedEc2Service(self, 'myWebApp-service',
+          cpu=256,
+          memory_limit_mib=512,
+          cluster=cluster,
+          desired_count=1,
+          service_name='myWebApp-service',
+          task_image_options={
+            "image": ecs.ContainerImage.from_ecr_repository(repository),
+            "container_port": 8080,
+          },
+          public_load_balancer=True
+        )
+
+        cdk.CfnOutput(self, 'ClusterArn', value=load_balanced_service.cluster.cluster_arn)
