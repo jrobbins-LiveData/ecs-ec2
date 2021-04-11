c41bef1aacd34f703661925c7f50b5d0876556c0
Jeff Robbins <jeffr@livedata.com>
2021-04-11 09:39:34 -0400

    still working on stuck CF deploy

--- ecs_ec2/ecs_ec2_stack.py
+++ ecs_ec2/ecs_ec2_stack.py
@@ -1,11 +1,11 @@
 # based on https://blog.kylegalbraith.com/2019/10/24/how-to-run-docker-containers-via-aws-elastic-container-service/
+from os import path
+
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_ecr as ecr
 from aws_cdk import aws_ecs as ecs
 from aws_cdk import aws_ecs_patterns as ecs_patterns
 from aws_cdk import core as cdk
-from aws_cdk.aws_ecr_assets import DockerImageAsset
-from os import path
 
 
 class EcsEc2Stack(cdk.Stack):
@@ -14,12 +14,7 @@ class EcsEc2Stack(cdk.Stack):
 
         # Docker build
         this_dir = path.dirname(__file__)
-        asset = DockerImageAsset(self, "myWebAppImage",
-          directory=path.join(this_dir, 'myWebApp')
-        )
-
-        # ECR repository
-        repository = asset.repository
+        asset = ecs.ContainerImage.from_asset(path.join(this_dir, 'myWebApp'))
 
         # ECS cluster/resources
         cluster = ecs.Cluster(self, 'myWebApp-cluster',
@@ -28,25 +23,29 @@ class EcsEc2Stack(cdk.Stack):
 
         # https://aws.amazon.com/ec2/graviton/
         # Free trial until June 30th 2021
+        # but not sure how to use!
         cluster.add_capacity('myWebApp-scaling-group',
           instance_type=ec2.InstanceType('t2.micro'), # TODO 't4g.micro'
-          desired_capacity=1
+          desired_capacity=1,
+          task_drain_time=cdk.Duration.seconds(0)
         )
+
 
         # https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ecs_patterns/ApplicationLoadBalancedEc2Service.html
         load_balanced_service = ecs_patterns.ApplicationLoadBalancedEc2Service(self, 'myWebApp-service',
-          cpu=256, # TODO
-          memory_limit_mib=512, # TODO
+          # cpu=256, # TODO
+          memory_limit_mib=1024, # TODO
           cluster=cluster,
           desired_count=1,
+          max_healthy_percent=100,
+          min_healthy_percent=0, # TODO daemon question
           service_name='myWebApp-service',
           task_image_options={
-            "image": ecs.ContainerImage.from_ecr_repository(repository),
-            # "container_port": 8080, # TODO
+            "image": asset,
+            "container_port": 80
           },
           public_load_balancer=True
         )
 
-        cdk.CfnOutput(self, 'ContainerRepository', value=repository.repository_uri)
         cdk.CfnOutput(self, 'ClusterArn', value=load_balanced_service.cluster.cluster_arn)
         cdk.CfnOutput(self, 'DnsName', value=load_balanced_service.load_balancer.load_balancer_dns_name)
